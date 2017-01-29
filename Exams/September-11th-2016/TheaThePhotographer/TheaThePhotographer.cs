ecc06ff888112d88e194c717b9035f8778ba95fd
gaydov <gaidov@gmail.com>
2017-01-29 14:06:12 +0200

    upload Exam September 11th 2016

---
+++ Exams/September-11th-2016/TheaThePhotographer/TheaThePhotographer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TheaThePhotographer
+{
+    public class TheaThePhotographer
+    {
+        public static void Main()
+        {
+            int picturesCount = int.Parse(Console.ReadLine());
+            int filterTimePerPic = int.Parse(Console.ReadLine());
+            int filterFactor = int.Parse(Console.ReadLine());
+            int uploadTimePerPic = int.Parse(Console.ReadLine());
+
+            double filteredPics = Math.Ceiling(picturesCount * ((double)filterFactor / 100));
+            long filterTime = (long)picturesCount * filterTimePerPic;
+            long uploadTime = (long)filteredPics * uploadTimePerPic;
+            long totalTime = filterTime + uploadTime;
+
+            TimeSpan resultTime = TimeSpan.FromSeconds(totalTime);
+
+            Console.WriteLine($"{resultTime.Days:D1}:{resultTime.Hours:D2}:{resultTime.Minutes:D2}:{resultTime.Seconds:D2}");
+        }
+    }
+}
