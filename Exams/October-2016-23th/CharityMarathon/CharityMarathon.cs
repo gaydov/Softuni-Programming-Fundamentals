177484ce1d5ab702a328e004c717b93c1d05fad3
gaydov <gaidov@gmail.com>
2017-02-04 19:14:16 +0200

    initial upload exam Oct 23 2016

---
+++ Exams/October-2016-23th/CharityMarathon/CharityMarathon.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CharityMarathon
+{
+    public class CharityMarathon
+    {
+        public static void Main()
+        {
+            int marathonLength = int.Parse(Console.ReadLine());
+            int runnersCount = int.Parse(Console.ReadLine());
+            int avgLaps = int.Parse(Console.ReadLine());
+            int lapLength = int.Parse(Console.ReadLine());
+            int trackCapacity = int.Parse(Console.ReadLine());
+            decimal moneyPerKm = decimal.Parse(Console.ReadLine());
+
+            int maximumRunners = trackCapacity * marathonLength;
+
+            if(runnersCount > maximumRunners)
+            {
+                runnersCount = maximumRunners;
+            }
+
+            long totalMeters = (long)runnersCount * avgLaps * lapLength;
+            long totalKms = totalMeters / 1000;
+            decimal moneyRaised = totalKms * moneyPerKm;
+
+            Console.WriteLine($"Money raised: {moneyRaised:F2}");
+        }
+    }
+}
---
+++ Exams/October-2016-23th/RoliTheCoder/RoliTheCoder.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace RoliTheCoder
+{
+    public class Event
+    {
+        public string Name { get; set; }
+        public List<string> Participants { get; set; }
+    }
+    public class RoliTheCoder
+    {
+        public static void Main()
+        {
+            Dictionary<int, Event> IDsWithEvents = new Dictionary<int, Event>();
+
+            Regex IDwithEventName = new Regex(@"[0-9]+ #\w+");
+            Regex participantsRegex = new Regex(@"@\S+");
+
+            string input = Console.ReadLine();
+
+            while (!input.Equals("Time for Code"))
+            {
+                Match IDwithEvent = IDwithEventName.Match(input);
+
+                if (IDwithEvent.Success)
+                {
+                    string[] idWithEventNameArr = IDwithEvent.ToString().Split(new char[] { ' ', '#' }, StringSplitOptions.RemoveEmptyEntries);
+                    int currentEventID = int.Parse(idWithEventNameArr[0]);
+                    string currentEventName = idWithEventNameArr[1];
+                    MatchCollection participantsMatches = participantsRegex.Matches(input);
+                    List<string> participants = participantsMatches.Cast<Match>().Select(m => m.Value).ToList();
+
+                    if (IDsWithEvents.ContainsKey(currentEventID))
+                    {
+                        if (IDsWithEvents[currentEventID].Name.Equals(currentEventName))
+                        {
+                            IDsWithEvents[currentEventID].Participants.AddRange(participants);
+                        }
+                        else
+                        {
+                            input = Console.ReadLine();
+                            continue;
+                        }
+                    }
+                    else
+                    {
+                        Event currentEvent = new Event();
+                        currentEvent.Name = currentEventName;
+                        currentEvent.Participants = participants;
+
+                        IDsWithEvents.Add(currentEventID, currentEvent);
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+
+            // If there are two or more participants with the same name for event we must distinct them:
+            foreach (Event eventEntry in IDsWithEvents.Values)
+            {
+                List<string> distinctParticipants = eventEntry.Participants.Distinct().ToList();
+                eventEntry.Participants = distinctParticipants;
+            }
+
+            foreach (KeyValuePair<int, Event> eventEntry in IDsWithEvents.OrderByDescending(e => e.Value.Participants.Count).ThenBy(e => e.Value.Name))
+            {
+                Console.WriteLine($"{eventEntry.Value.Name} - {eventEntry.Value.Participants.Count()}");
+
+                foreach (string participant in eventEntry.Value.Participants.OrderBy(p => p))
+                {
+                    Console.WriteLine(participant);
+                }
+            }
+        }
+    }
+}
