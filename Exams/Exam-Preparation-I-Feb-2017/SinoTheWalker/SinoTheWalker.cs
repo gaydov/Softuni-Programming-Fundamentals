f725b9003c62c236088ae6296e6cb9a7bd18c579
gaydov <gaidov@gmail.com>
2017-02-21 15:43:38 +0200

    initial upload

---
+++ Exams/Exam-Preparation-I-Feb-2017/SinoTheWalker/SinoTheWalker.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SinoTheWalker
+{
+    public class SinoTheWalker
+    {
+        public static void Main()
+        {
+            DateTime leavingTime = DateTime.ParseExact(Console.ReadLine(), "HH:mm:ss", null);
+            int stepsCount = int.Parse(Console.ReadLine());
+            int timePerStep = int.Parse(Console.ReadLine());
+
+            long walkingTime = (stepsCount % 86400) * (timePerStep % 86400);
+            DateTime arrivalTime = leavingTime.AddSeconds(walkingTime);
+
+            Console.WriteLine($"Time Arrival: {arrivalTime.Hour:D2}:{arrivalTime.Minute:D2}:{arrivalTime.Second:D2}");
+        }
+    }
+}
---
+++ Exams/Exam-Preparation-I-Feb-2017/WinningTicket/WinningTicket.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace WinningTicket
+{
+    public class WinningTicket
+    {
+        public static void Main()
+        {
+            string[] inputTickets = Regex.Split(Console.ReadLine(), @"\s*,\s+");
+            Dictionary<string, string> ticketsAndAwards = new Dictionary<string, string>();
+
+            string winnerPattern = @"[@]{6,9}|[#]{6,9}|[$]{6,9}|[\^]{6,9}";
+            string jackpotPattern = @"[@]{10}|[#]{10}|[$]{10}|[\^]{10}";
+
+            foreach (string ticket in inputTickets)
+            {
+                if (ticket.Length != 20)
+                {
+                    ticketsAndAwards.Add(ticket, "invalid ticket");
+                }
+                else
+                {
+                    string leftPart = ticket.Substring(0, 10);
+                    string rightPart = ticket.Substring(10, 10);
+
+                    Match jackpotLeft = Regex.Match(leftPart, jackpotPattern);
+                    Match jackpotRight = Regex.Match(rightPart, jackpotPattern);
+
+                    Match winnerLeft = Regex.Match(leftPart, winnerPattern);
+                    Match winnerRight = Regex.Match(rightPart, winnerPattern);
+
+                    if (jackpotLeft.Success && jackpotRight.Success && jackpotLeft.ToString()[0].Equals(jackpotRight.ToString()[0]))
+                    {
+                        char winningSymbol = jackpotLeft.ToString()[0];
+
+                        ticketsAndAwards.Add(ticket, $"10{winningSymbol} Jackpot!");
+                    }
+                    else if (winnerLeft.Success && winnerRight.Success && winnerLeft.ToString()[0].Equals(winnerRight.ToString()[0]))
+                    {
+                        char winningSymbol = winnerLeft.ToString()[0];
+                        int prize = Math.Min(winnerLeft.Length, winnerRight.Length);
+
+                        ticketsAndAwards.Add(ticket, prize.ToString() + winningSymbol);
+                    }
+                    else
+                    {
+                        ticketsAndAwards.Add(ticket, "no match");
+                    }
+                }
+            }
+
+            foreach (KeyValuePair<string, string> ticket in ticketsAndAwards)
+            {
+                if (ticket.Value.Equals("invalid ticket"))
+                {
+                    Console.WriteLine("invalid ticket");
+                }
+                else
+                {
+                    Console.WriteLine($"ticket \"{ticket.Key}\" - {ticket.Value}");
+                }
+            }
+        }
+    }
+}
